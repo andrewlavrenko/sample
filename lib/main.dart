8b1836d7c4dd33ca7c98af4b9c5e3a835d009f66
andrewlavrenko <alavrenko33@gmail.com>
2022-12-28 21:57:21 +0200

    added localization i18n

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:sample/routes/router.gr.dart';
 
 import 'core/injectable/injectable_init.dart';
 import 'generated/colors.gen.dart';
+import 'generated/i18n.dart';
 
 void main() {
   configureDependencies();
@@ -18,6 +20,14 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp]);
     return MaterialApp.router(
+      localizationsDelegates: const [
+        I18n.delegate,
+        GlobalMaterialLocalizations.delegate,
+        GlobalCupertinoLocalizations.delegate,
+      ],
+      supportedLocales: I18n.delegate.supportedLocales,
+      localeResolutionCallback: I18n.delegate.resolution(),
+      onGenerateTitle: (context) => I18n.of(context).appName,
       debugShowCheckedModeBanner: false,
       routerDelegate: _appRouter.delegate(),
       routeInformationParser: _appRouter.defaultRouteParser(),
